ca66898674eb3334ad76ec1216ae2b75c3127359
Nathnael Mesfin <73286501+Nathnaelmesfin@users.noreply.github.com>
2023-05-27 00:01:21 +0300

    Add files via upload

---
+++ typing.js
@@ -0,0 +1,26 @@
+var data = [
+    {
+      AboutDevTypeText: "<span>HI! I'm Nathnael<br/><br/>I'm a Front-End Web Developer.</span>"
+    }
+  ];
+  
+  var allElements = document.getElementsByClassName("typeing");
+  for (var j = 0; j < allElements.length; j++) {
+    var currentElementId = allElements[j].id;
+    var currentElementIdContent = data[0][currentElementId];
+    var element = document.getElementById(currentElementId);
+    var devTypeText = currentElementIdContent;
+  
+    // type code
+    var i = 0, isTag, text;
+    (function type() {
+      text = devTypeText.slice(0, ++i);
+      if (text === devTypeText) return;
+      element.innerHTML = text + `<span class='blinker'>&#32;</span>`;
+      var char = text.slice(-1);
+      if (char === "<") isTag = true;
+      if (char === ">") isTag = false;
+      if (isTag) return type();
+      setTimeout(type, 60);
+    })();
+  }
